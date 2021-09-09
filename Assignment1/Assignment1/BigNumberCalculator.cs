8d8a7fff6fa0f92e04409ef74da618731867ef91
Ja_Hwang <dmagk560@gmail.com>
2021-09-09 23:47:02 +0900

    Add class BigNumberCalculator and EMode

---
+++ Assignment1/Assignment1/BigNumberCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment1
+{
+    public class BigNumberCalculator
+    {
+        public BigNumberCalculator(int bitCount, EMode mode)
+        {
+
+        }
+
+        public static string GetOnesComplementOrNull(string num)
+        {
+            if (num[0] != '0' || num[1] != 'b')
+            {
+                return null;
+            }
+
+            string tempBinary = num.Substring(2);
+            int inpuBinary = int.Parse(tempBinary);
+
+            return null;
+        }
+    }
+}
