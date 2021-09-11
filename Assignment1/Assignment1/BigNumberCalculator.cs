0e74a760efd2024fb15254b7bf40c0d31f52961e
junghyunhwang <dmagk560@gmail.com>
2021-09-11 22:17:32 +0900

    Add GetTwosComplement and ToBinaryOrNull

--- Assignment1/Assignment1/BigNumberCalculator.cs
+++ Assignment1/Assignment1/BigNumberCalculator.cs
@@ -11,7 +11,7 @@ namespace Assignment1
 
         }
 
-        private static int ConvertToDecimal(string num)
+        private static int ConvertBinaryToDecimal(string num)
         {
             int index = 1;
             int resultInt = 0;
@@ -23,15 +23,40 @@ namespace Assignment1
                     resultInt += index;
                 }
 
-                index <<= 1;
+                index *= 2;
             }
 
             return resultInt;
         }
 
+        private static string ConvertDecimalToBinary(int num)
+        {
+            if (num == 0)
+            {
+                return "0000";
+            }
+
+            StringBuilder binary = new StringBuilder();
+
+            while(num > 1)
+            {
+                int bit = num % 2;
+                binary.Insert(0, bit);
+                num /= 2;
+            }
+
+            binary.Insert(0, num);
+
+            return binary.ToString();
+        }
+
         public static string GetOnesComplementOrNull(string num)
         {
-            if (num[0] != '0' || num[1] != 'b')
+            if (num.Length <= 2)
+            {
+                return null;
+            }
+            else if (num[0] != '0' || num[1] != 'b')
             {
                 return null;
             }
@@ -64,6 +89,84 @@ namespace Assignment1
             }
 
             return resultString.ToString();
+        }
+
+        public static string GetTwosComplementOrNull(string num)
+        {
+            if (num[0] != '0' || num[1] != 'b')
+            {
+                return null;
+            }
+
+            for (int i = 2; i < num.Length; i++)
+            {
+                if (num[i] != '0' && num[i] != '1')
+                {
+                    return null;
+                }
+            }
+
+            string inputBinary = num.Substring(2);
+            StringBuilder resultString = new StringBuilder(num.Length);
+
+            foreach (var bit in inputBinary)
+            {
+                switch (bit)
+                {
+                    case '0':
+                        resultString.Append('1');
+                        break;
+                    case '1':
+                        resultString.Append('0');
+                        break;
+                    default:
+                        Debug.Assert(false, "Wrong input binary");
+                        break;
+                }
+            }
+
+            int resultDecimal = ConvertBinaryToDecimal(resultString.ToString()) + 1;
+            string resultbinary = ConvertDecimalToBinary(resultDecimal);
+
+            return $"0b{resultbinary}";
+        }
+
+        public static string ToBinaryOrNull(string num)
+        {
+            int inputDecimal;
+            bool bIsNum = Int32.TryParse(num, out inputDecimal);
+
+            if (bIsNum)
+            {
+                string binary = ConvertDecimalToBinary(inputDecimal); // 자리 계산 해야함
+
+                return binary;
+            }
+
+            string profix = num.Substring(0, 2);
+            string resultValue = "";
+
+            switch(profix)
+            {
+                case "0b":
+                    for (int i = 2; i < num.Length; i++)
+                    {
+                        if (num[i] != '0' && num[i] != '1')
+                        {
+                            return null;
+                        }
+                    }
+
+                    resultValue = num;
+                    break;
+                case "0x":
+                    
+                    break;
+                default:
+                    return null;
+            }
+
+            return resultValue;
         }
     }
 }
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Assignment1
 {
@@ -6,8 +7,29 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            string test = BigNumberCalculator.GetOnesComplementOrNull("0b0110101011101011100000");
-            Console.WriteLine(test);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("as89fdf0") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0xFAKEHEX") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0bFAKEBINARY") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("FAKEDECIMAL") == null);
+
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0x") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0b") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("    ") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("-") == null);
+
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("-10") == null);
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0xFC34") == null);
+
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0b0000111010110") == "0b1111000101001");
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0b1000") == "0b0111");
+            Debug.Assert(BigNumberCalculator.GetOnesComplementOrNull("0b0110101011101011100000") == "0b1001010100010100011111");
+
+            Debug.Assert(BigNumberCalculator.GetTwosComplementOrNull("0b0000111010110") == "0b1111000101010");
+            Debug.Assert(BigNumberCalculator.GetTwosComplementOrNull("0b1000") == "0b1000");
+            Debug.Assert(BigNumberCalculator.ToBinaryOrNull("123") == "0b01111011");
+
+            Console.WriteLine("No prob");
         }
     }
 }
