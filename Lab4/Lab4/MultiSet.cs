9691f1b45994c617e02b6803ae2604181fa6454e
junghyunhwang <dmagk560@gmail.com>
2021-09-28 18:55:30 +0900

    Add Lab 4

---
+++ Lab4/Lab4/MultiSet.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace Lab4
+{
+    public sealed class MultiSet
+    {
+        private List<string> set = new List<string>(256);
+        private Dictionary<string, uint> multiplicity = new Dictionary<string, uint>();
+
+        public void Add(string element)
+        {
+            if (set.Contains(element))
+            {
+                multiplicity[element]++;
+            }
+            else
+            {
+                multiplicity.Add(element, 1);
+            }
+
+            set.Add(element);
+        }
+
+        public bool Remove(string element)
+        {
+            if (multiplicity.ContainsKey(element))
+            {
+                multiplicity[element]--;
+            }
+
+            return set.Remove(element);
+        }
+
+        public uint GetMultiplicity(string element)
+        {
+            if (!multiplicity.ContainsKey(element))
+            {
+                return 0;
+            }
+
+            return multiplicity[element];
+        }
+
+        public List<string> ToList()
+        {
+            set.Sort();
+            return set;
+        }
+
+        public MultiSet Union(MultiSet other)
+        {
+            List<string> otherList = other.ToList();
+            MultiSet resultSet = new MultiSet();
+
+            return null;
+        }
+
+        public MultiSet Intersect(MultiSet other)
+        {
+            return null;
+        }
+
+        public MultiSet Subtract(MultiSet other)
+        {
+            return null;
+        }
+
+        public List<MultiSet> FindPowerSet()
+        {
+            return null;
+        }
+
+        public bool IsSubsetOf(MultiSet other)
+        {
+            return false;
+        }
+
+        public bool IsSupersetOf(MultiSet other)
+        {
+            return false;
+        }
+    }
+}
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab4
 {
@@ -7,6 +8,34 @@ namespace Lab4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MultiSet set = new MultiSet();
+            set.Add("a");
+            set.Add("b");
+            set.Add("c");
+            set.Add("a");
+            set.Add("a");
+
+            uint mul = set.GetMultiplicity("a");
+            Console.WriteLine(mul);
+
+            bool tryRemove = set.Remove("a");
+            Console.WriteLine(tryRemove);
+
+            mul = set.GetMultiplicity("a");
+            Console.WriteLine(mul);
+
+            tryRemove = set.Remove("d");
+            Console.WriteLine(tryRemove);
+
+            MultiSet set1 = new MultiSet();
+
+            set1.Add("apple");
+            set1.Add("watermelon");
+            set1.Add("plum");
+            set1.Add("apple");
+
+            List<string> list = set1.ToList();
+            Console.WriteLine(list);
         }
     }
 }
