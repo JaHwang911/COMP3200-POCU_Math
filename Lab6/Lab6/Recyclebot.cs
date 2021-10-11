e5258246c9d0307015d4ea0de0a24e6b10025d70
junghyunhwang <dmagk560@gmail.com>
2021-10-11 17:27:56 +0900

    Add Lab 6

---
+++ Lab6/Lab6/Recyclebot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Lab6
+{
+    public class Recyclebot
+    {
+        public List<Item> RecycleItems { get; private set; } 
+        public List<Item> NonRecycleItems { get; private set; }
+
+        public Recyclebot()
+        {
+            RecycleItems = new List<Item>();
+            NonRecycleItems = new List<Item>();
+        }
+
+        public void Add(Item item)
+        {
+            switch (item.Type)
+            {
+                case EType.Paper:
+                case EType.Furniture:
+                case EType.Electronics:
+                    if (2.0 <= item.Weight && item.Weight < 5.0)
+                    {
+                        RecycleItems.Add(item);
+                    }
+                    else
+                    {
+                        NonRecycleItems.Add(item);
+                    }
+                    break;
+                case EType.Compost:
+                case EType.Glass:
+                case EType.Plastic:
+                    RecycleItems.Add(item);
+                    break;
+                default:
+                    Debug.Assert(false, "Wrong item type");
+                    break;
+            }
+        }
+
+        public List<Item> Dump()
+        {
+            List<Item> dumpResult = new List<Item>(NonRecycleItems.Count);
+
+            foreach (var item in NonRecycleItems)
+            {
+                if (item.IsToxicWaste)
+                {
+                    if (item.Type == EType.Furniture || item.Type == EType.Electronics)
+                    {
+                        dumpResult.Add(item);
+                    }
+                }
+                else
+                {
+                    dumpResult.Add(item);
+                }
+            }
+
+            return dumpResult;
+        }
+    }
+}
