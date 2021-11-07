3c954874e6b42f5ed12ecf5e73b4a0b26f37b7f1
junghyunhwang <dmagk560@gmail.com>
2021-11-08 01:51:53 +0900

    Add Noise in recursive

---
+++ Assignment3/Assignment3/SineNoise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment3
+{
+    public sealed class SineNoise : INoise
+    {
+        private const double BASE_SAMPLING_WIDTH = Math.PI / 4;
+        private double mX = -BASE_SAMPLING_WIDTH;
+
+        public int GetNext(int level)
+        {
+            mX += BASE_SAMPLING_WIDTH / Math.Pow(2, level);
+            int result = (int)(5 * Math.Sin(mX));
+            return result;
+        }
+    }
+}
--- Assignment3/Assignment3/StepMaker.cs
+++ Assignment3/Assignment3/StepMaker.cs
@@ -5,23 +5,30 @@ namespace Assignment3
 {
     public static class StepMaker
     {
-        public static List<int> MakeSteps(int[] steps)
+        public static List<int> MakeSteps(int[] steps, INoise noise)
         {
             List<int> resultSteps = new List<int>();
             double[] newStepDistanceAmout = new double[5];
+            //double[] reverseDistanceAmout = new double[5];
             int index = 0;
-
+            
             resultSteps.AddRange(steps);
+
             newStepDistanceAmout[0] = 0.8;
             newStepDistanceAmout[1] = 0.75;
             newStepDistanceAmout[2] = 0.67;
             newStepDistanceAmout[3] = 0.5;
-            
+
+            //reverseDistanceAmout[0] = 0.2;
+            //reverseDistanceAmout[1] = 0.25;
+            //reverseDistanceAmout[2] = 0.33;
+            //reverseDistanceAmout[3] = 0.5;
+
             while (index < resultSteps.Count)
             {
                 if (index + 1 < resultSteps.Count && (resultSteps[index + 1] - resultSteps[index]) > 10)
                 {
-                    resultSteps.InsertRange(index + 1, AddStepsRecursive(newStepDistanceAmout, 0, resultSteps[index], resultSteps[index + 1]));
+                    resultSteps.InsertRange(index + 1, AddStepsRecursive(newStepDistanceAmout, 0, resultSteps[index], resultSteps[index + 1], noise));
                 }
                 else
                 {
@@ -32,7 +39,7 @@ namespace Assignment3
             return resultSteps;
         }
 
-        private static List<int> AddStepsRecursive(double[] newStepDistanceAmout, int recursiveLevel, int min, double max)
+        public static List<int> AddStepsRecursive(double[] newStepDistanceAmout, int recursiveLevel, int min, double max, INoise noise)
         {
             List<int> steps = new List<int>();
 
@@ -44,9 +51,29 @@ namespace Assignment3
             double newStepNumber = (max - min) * newStepDistanceAmout[recursiveLevel];
 
             max = min + newStepNumber;
+            int noiseStep = noise.GetNext(recursiveLevel); // 위치 중요
             recursiveLevel++;
-            steps = AddStepsRecursive(newStepDistanceAmout, recursiveLevel, min, max);
-            steps.Add((int)max);
+            steps = AddStepsRecursive(newStepDistanceAmout, recursiveLevel, min, max, noise);
+            steps.Add((int)max + noiseStep);
+
+            return steps;
+        }
+
+        public static List<int> ver2AddStepsRecursive(double[] newStepDistanceAmout, int recursiveLevel, int min, double max, INoise noise)
+        {
+            List<int> steps = new List<int>();
+
+            if (recursiveLevel == newStepDistanceAmout.Length - 1)
+            {
+                return steps;
+            }
+
+            double newStepNumber = (max - min) * newStepDistanceAmout[recursiveLevel];
+
+            max = min + newStepNumber;
+            steps = AddStepsRecursive(newStepDistanceAmout, recursiveLevel + 1, min, max, noise);
+            int noiseStep = noise.GetNext(recursiveLevel); // 위치 중요
+            steps.Add((int)max + noiseStep);
 
             return steps;
         }
