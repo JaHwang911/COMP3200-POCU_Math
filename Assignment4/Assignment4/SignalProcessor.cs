bf410318cd0a23e8e7610da0dd932c035b052f38
Ja_Hwang <dmagk560@gmail.com>
2021-11-30 22:59:54 +0900

    Add Convolve1D

--- Assignment4/Assignment4/SignalProcessor.cs
+++ Assignment4/Assignment4/SignalProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Assignment4
@@ -16,18 +17,49 @@ namespace Assignment4
             {
                 double variable = firstValue + i;
                 double coefficient = 1 / (sigma * Math.Sqrt(2 * Math.PI));
-                double jae = (variable * variable) / (2.0 * powSigma) * -1;
-                double term = Math.Exp(jae);
+                double pow = (variable * variable) / (2.0 * powSigma) * -1;
+                double term = Math.Exp(pow);
                 gaussianResult[i] = coefficient * term;
             }
 
-            Console.WriteLine(string.Join(",", gaussianResult));
             return gaussianResult;
         }
 
         public static double[] Convolve1D(double[] signal, double[] filter)
         {
-            return null;
+            List<double> Convolved = new List<double>();
+            List<double> tempSignal = new List<double>(256);
+            List<double> tempFilter = new List<double>(filter.Length);
+            int extraElementCount = filter.Length % 2 != 0 ? filter.Length / 2 : filter.Length / 2 + 1;
+
+            Array.Reverse(filter);
+            tempSignal.AddRange(signal);
+            tempFilter.AddRange(filter);
+
+            for (int i = 0; i < extraElementCount; i++)
+            {
+                tempSignal.Insert(0, 0);
+            }
+
+            for (int i = 0; i < extraElementCount; i++)
+            {
+                tempSignal.Add(0);
+            }
+
+            for (int i = tempFilter.Count - extraElementCount - 1; i < tempSignal.Count - extraElementCount; i++)
+            {
+                double value = 0;
+
+                for (int j = 0; j < tempFilter.Count; j++)
+                {
+                    int index = i + j - extraElementCount;
+                    value += tempFilter[j] * tempSignal[index];
+                }
+
+                Convolved.Add(value);
+            }
+
+            return Convolved.ToArray();
         }
 
         public static double[,] GetGaussianFilter2D(double sigma)
